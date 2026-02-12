11bb74cd9f59c001474c5f3578f75a52b5cf7bd4
Hamed Tabatabaeifar <hamedtabafar@gmail.com>
2026-02-13 01:02:49 +0330

    adding neccesary cli functions that will do the comparison

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+import tempfile
+import os
+from plagiarism.cli import compare_hierarchies, compare_two_files, load_config
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+if __name__ == '__main__':
+    app.run(debug=True)
