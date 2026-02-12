d1c94e8b0c35ac9af96f890f389f25d9fa5e2a53
Hamed Tabatabaeifar <hamedtabafar@gmail.com>
2026-02-13 02:09:41 +0330

    selecting port 5000 for flask application

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def compare():
             if os.path.exists(p): os.remove(p)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
